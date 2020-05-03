a66d2acdff0c6fcc2702346928a308138503d559
Nick Vovk <dyvniy@yandex.ru>
2020-05-03 07:42:57 -0400

    fix log

--- python/example/test_simple.py
+++ python/example/test_simple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-from vosk import Model, KaldiRecognizer, SetLogLevel
+from vosk import Model, KaldiRecognizer #, SetLogLevel
 import sys
 import os
 import wave
 
-SetLogLevel(0)
+#SetLogLevel(0)
 
 if not os.path.exists("model"):
     print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
