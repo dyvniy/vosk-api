ccc9779789b02c6c3cdd04b1df81b8e95bc4d2d3
Nick Vovk <dyvniy@yandex.ru>
2020-10-02 07:53:28 -0400

    serv decode

--- python/example/webServ.py
+++ python/example/webServ.py
@@ -1,16 +1,47 @@
+#!/usr/bin/python3
+
+from vosk import Model, KaldiRecognizer #, SetLogLevel
+import sys
+import os
+import wave
+import time
+
 import tornado.ioloop
 import tornado.web
 
+#SetLogLevel(0)
+model = Model("model")
+
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write("Hell")   
+    tm = time.time();
+    self.write("Hell\n")   
+    self.write(str(self.recognize("./test.wav")))
+    self.write(str(time.time() - tm))
   def post(self):
     sepf.write("post")
+  def prepare(self):
+    pass
+    
+  def recognize(self, namepath = "./test.wav"):
+    wf = wave.open(namepath, "rb")
+    rec = KaldiRecognizer(model, wf.getframerate())
+    while True:
+      data = wf.readframes(4000)
+      if len(data) == 0:
+        break
+      if rec.AcceptWaveform(data):
+        print(rec.Result())
+      else:
+        print(rec.PartialResult())
+
+    return (rec.FinalResult())
 
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
   ])
+  
   
 if __name__ == "__main__":
   app = make_app()
